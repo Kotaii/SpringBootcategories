daf7ab5960ce00be26f453dc76eb5fd57be41eb5
Kotaii <ronaldkwong@RONALDs-MacBook-Air.local>
2022-12-25 09:15:32 -0800

    first commit

---
+++ src/main/java/com/springboot/test/LoadDatabase.java
@@ -0,0 +1,15 @@
+package com.springboot.test;
+
+
+
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+   
+    
+}
